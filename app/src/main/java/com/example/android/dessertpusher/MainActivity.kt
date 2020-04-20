c0e824ee82ba2c103edf566ba8cca5a7aa2d11ff
Hannah Olukoye <hannah.olukoye@macro-eyes.com>
2020-04-20 15:50:45 +0300

    Feat: Setup and teardown solution

--- app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -30,6 +30,7 @@ import timber.log.Timber
 
 class MainActivity : AppCompatActivity(), LifecycleObserver {
 
+    private lateinit var dessertTimer: DessertTimer
     private var revenue = 0
     private var dessertsSold = 0
 
@@ -74,7 +75,8 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
             onDessertClicked()
         }
 
-        // TODO (02) Create a DessertTimer
+         dessertTimer = DessertTimer()
+
 
         // Set the TextViews to the right values
         binding.revenue = revenue
@@ -152,10 +154,11 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
     }
 
     /** Lifecycle Methods **/
-    // TODO (03) Start the DessertTimer in onStart and stop the timer in onStop
 
     override fun onStart() {
         super.onStart()
+        dessertTimer.startTimer()
+
         Timber.i("onStart Called")
     }
 
@@ -171,6 +174,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     override fun onStop() {
         super.onStop()
+        dessertTimer.stopTimer()
         Timber.i("onStop Called")
     }
 
